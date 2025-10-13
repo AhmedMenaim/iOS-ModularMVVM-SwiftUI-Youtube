291ba0c14fc7d98352bc543bfdfd56a490adb3a0
AhmedMenaim <ahmedmenaim8@gmail.com>
2025-10-13 12:42:00 +0300

    Initial Commit

---
+++ iOS-ModularMVVM-SwiftUI-Youtube/iOS_ModularMVVM_SwiftUI_YoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ModularMVVM_SwiftUI_YoutubeApp.swift
+//  iOS-ModularMVVM-SwiftUI-Youtube
+//
+//  Created by Menaim on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ModularMVVM_SwiftUI_YoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
